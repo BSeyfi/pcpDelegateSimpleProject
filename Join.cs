41e49bcea1d86013697ec86f103ed1e8a29440a4
BSeyfi <42473071+BSeyfi@users.noreply.github.com>
2023-03-10 20:34:22 +0330

    initial commit

---
+++ Join.cs
@@ -0,0 +1,18 @@
+﻿namespace DelegateSimplePrograms;
+
+internal delegate int JoinNumbersDelegate(int operand1, int operand2);
+
+public static class Join
+{
+    public static int JoinNumber(int leftNumber, int rightNumber)
+    {
+        Console.WriteLine("Joining two integer numbers...");
+        return Convert.ToInt32($"{leftNumber}{rightNumber}");
+    }
+
+    public static string JoinNumber(string leftNumber, string rightNumber)
+    {
+        Console.WriteLine("Joining two string numbers...");
+        return $"{leftNumber}{rightNumber}";
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,7 @@
+﻿using System.Threading.Channels;
+using DelegateSimplePrograms;
+
+JoinNumbersDelegate joinNumbersDelegate = Join.JoinNumber;
+
+int joinedNumber=joinNumbersDelegate(10,20);
+Console.WriteLine(joinedNumber);
